431667a206047587db68332ab8c486499258a2d3
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2023-03-19 21:17:43 +0700

    update contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import './Contact.scss'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className='contact'>
+            {/* <div className='github'>
+                <img src="../../assets/github-mark.png" alt="" width="200px"/>
+            </div>
+            <div>Linkedln</div>
+            <div>Facebook</div>
+            <div>Instagram</div> */}
+            <li>
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" width="100px" height="auto"/>
+                <h3>Github</h3>
+            </li>
+            <li>
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" width="100px" height="auto"/>
+                <h3>Github</h3>
+            </li>
+            <li>
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" width="100px" height="auto"/>
+                <h3>Github</h3>
+            </li>
+            <li>
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" width="100px" height="auto"/>
+                <h3>Github</h3>
+            </li>
+        </div>
     )
 }
 
